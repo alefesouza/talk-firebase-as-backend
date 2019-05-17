131a280e2e697ea8b3aec790311697988bd9804f
Alefe Souza <contact@alefesouza.com>
2019-05-16 21:06:10 -0300

    feat: add BackEndBr talk

--- README.md
+++
@@ -1,3 +0,0 @@
-# talk-firebase-as-backend
-
-This repository contains a very simple example of Firebase Authentication with vanilla JavaScript, presented for the first time on [7Masters: Serverless](https://setemasters.imasters.com.br/edicoes/serverless/).
---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCruLbMzH9peWkSMYmtDuwNqK3vJnOtQro",
+    authDomain: "talk-firebase.firebaseapp.com",
+    databaseURL: "https://talk-firebase.firebaseio.com",
+    projectId: "talk-firebase",
+    storageBucket: "talk-firebase.appspot.com",
+    messagingSenderId: "305711011509",
+    appId: "1:305711011509:web:223d6ca22f6f929e"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
